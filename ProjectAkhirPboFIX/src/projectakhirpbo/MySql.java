a76dba955ad1c5c7373501db8832cab938cdb26d
armandsaf <77561798+armandsaf@users.noreply.github.com>
2021-01-19 23:06:35 +0700

    Update MySql.java

--- ProjectAkhirPboFIX/src/projectakhirpbo/MySql.java
+++ ProjectAkhirPboFIX/src/projectakhirpbo/MySql.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projectakhirpbo;
 
 import com.mysql.jdbc.Connection;
@@ -16,10 +11,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author muhammad yusuf
- */
 public class MySql {
     Connection konek = null;
     

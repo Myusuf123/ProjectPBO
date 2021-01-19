3ba9047008e24a3ef1cf2fd0cf1b74b00c1c9010
armandsaf <77561798+armandsaf@users.noreply.github.com>
2021-01-19 22:58:43 +0700

    Update DataJne.java

--- ProjectAkhirPboFIX/src/projectakhirpbo/DataJne.java
+++ ProjectAkhirPboFIX/src/projectakhirpbo/DataJne.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package projectakhirpbo;
 
-/**
- *
- * @author muhammad yusuf
- */
 public class DataJne {
- int noresi;
+int noresi;
 String namapenerima;
 String nohp;
 String alamat,jenisbarang;

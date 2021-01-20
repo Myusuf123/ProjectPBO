764cbdc323a32939f8adf9c8fc9c2180743c52f7
mcdhelby <77681871+mcdhelby@users.noreply.github.com>
2021-01-20 10:42:09 +0700

    Update FXMLDocumentController.java

--- ProjectAkhirPbo_part1/src/projectakhirpbo/FXMLDocumentController.java
+++ ProjectAkhirPbo_part1/src/projectakhirpbo/FXMLDocumentController.java
@@ -16,7 +16,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javax.swing.JOptionPane;
 
-
 public class FXMLDocumentController implements Initializable {
     
     @FXML
